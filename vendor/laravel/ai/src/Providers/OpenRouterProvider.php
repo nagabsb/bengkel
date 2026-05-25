3f6f7c1414955e591e85b417572ea9c0cff01928
najmi <najmi@MacBook-Air-najmi.local>
2026-05-25 10:45:01 +0700

    include vendor for vercel

---
+++ vendor/laravel/ai/src/Providers/OpenRouterProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Laravel\Ai\Providers;
+
+use Laravel\Ai\Contracts\Providers\TextProvider;
+use Laravel\Ai\Contracts\Providers\EmbeddingProvider;
+
+class OpenRouterProvider extends Provider implements EmbeddingProvider, TextProvider
+{
+    use Concerns\GeneratesText;
+    use Concerns\HasTextGateway;
+    use Concerns\StreamsText;
+    use Concerns\GeneratesEmbeddings;
+    use Concerns\HasEmbeddingGateway;
+
+
+    /**
+     * Get the name of the default text model.
+     */
+    public function defaultTextModel(): string
+    {
+        return $this->config['models']['text']['default'] ?? 'anthropic/claude-sonnet-4.5';
+    }
+
+    /**
+     * Get the name of the cheapest text model.
+     */
+    public function cheapestTextModel(): string
+    {
+        return $this->config['models']['text']['cheapest'] ?? 'anthropic/claude-haiku-4.5';
+    }
+
+    /**
+     * Get the name of the smartest text model.
+     */
+    public function smartestTextModel(): string
+    {
+        return $this->config['models']['text']['smartest'] ?? 'anthropic/claude-opus-4.5';
+    }
+
+    /**
+     * Get the name of the default embeddings model.
+     */
+    public function defaultEmbeddingsModel(): string
+    {
+        return $this->config['models']['embeddings']['default'] ?? 'google/gemini-embedding-001';
+    }
+
+    /**
+     * Get the default dimensions of the default embeddings model.
+     */
+    public function defaultEmbeddingsDimensions(): int
+    {
+        return $this->config['models']['embeddings']['dimensions'] ?? 1536;
+    }
+}
